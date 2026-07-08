9faf0fc521d4342130bfed0bde40f32a0297290b
pisalim44 <thiagodemourajesus@gmail.com>
2026-07-08 14:49:25 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ==========================================
+// ARQUIVO DE CONFIGURAÇÃO DE AMBIENTE
+// Altere a URL abaixo apontando para o seu Apps Script implantado
+// ==========================================
+window.PISAPP_API_URL = 'https://script.google.com/macros/s/AKfycbwXlcTgqyhKJxlpPNYv7OGk08iKmmXvUUtcV8nblqoYGNXZpSb5H6Td0iDxbDpyUCNO/exec';
+const PISAPP_API_URL = window.PISAPP_API_URL;
